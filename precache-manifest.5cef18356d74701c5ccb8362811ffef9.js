b9a497b2de6eac16970313a6a8b59cccdf94fc24
sisaki-n <sisaki.n@gmail.com>
2019-09-01 00:25:13 +0530

    Updates

---
+++ precache-manifest.5cef18356d74701c5ccb8362811ffef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b321a6b71379944191a4d799c69f1dd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a140d033758e5e4d9528",
+    "url": "/monsters-rolodex/static/css/main.d19af1df.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "a140d033758e5e4d9528",
+    "url": "/monsters-rolodex/static/js/main.9f767fda.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
